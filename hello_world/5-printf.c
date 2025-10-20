94bfa3b24cb7fc05ddcc9631802f8cc6a14b60a0
veroniquebvs <veronique@DESKTOP-2QQLIC1.localdomain>
2025-10-20 21:57:26 +0200

    write a C program that prints a sentence with printf

---
+++ hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+* main - point d'entrée du programme
+* Description : Affiche une phrase à l'aide de la fonction printf
+* Return: 0 si le programme s'exécute correctement
+*/
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
+}
