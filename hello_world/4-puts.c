c07d30fed81308259c9a82dc17cd54df1c2726f1
veroniquebvs <veronique@DESKTOP-2QQLIC1.localdomain>
2025-10-20 21:27:35 +0200

    write C program that prints a sentence

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -6,6 +6,6 @@
 */
 int main(void)
 {
-puts("La programmation, c'est comme construire un puzzle multilingue");
+puts("\"La programmation, c'est comme construire un puzzle multilingue");
 return (0);
 }
