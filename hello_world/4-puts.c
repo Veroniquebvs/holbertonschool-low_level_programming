ad87e7be6658df6cf0e9c88789bd543bd60c058e
veroniquebvs <veronique@DESKTOP-2QQLIC1.localdomain>
2025-10-20 21:24:05 +0200

    write C program that prints a sentence

---
+++ hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+* main - point d'entrée du programme
+* Description : Affiche une phrase à l'aide de la fonction puts
+* Return: 0 si le programme s'exécute correctement
+*/
+int main(void)
+{
+puts("La programmation, c'est comme construire un puzzle multilingue");
+return (0);
+}
