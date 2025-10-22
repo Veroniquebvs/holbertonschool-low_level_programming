440cd33433dc5bdb891ee15bf22b501433fd665a
veroniquebvs <beauvais.veronique@gmail.com>
2025-10-22 20:31:25 +0200

    create c programm that prints _putchar

---
+++ functions_nested_loops/0-putchar.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * main - point d'entrée du programme
+ * Description : affiche les lettres de l'alphabet
+ * Return: 0 si le programme s'exécute correctement
+ */
+
+int main(void)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+
+	return (0);
+}
