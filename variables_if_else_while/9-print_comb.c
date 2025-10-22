c4b3297dca31f554f92792375ffd505c68cc81fb
veroniquebvs <beauvais.veronique@gmail.com>
2025-10-22 11:11:27 +0200

    create programm that prints all single digit

---
+++ variables_if_else_while/9-print_comb.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - point d'entrée du programme
+ * Description: affiche nombre à un chiffre
+ * Return: 0 si le programme s'exécute correctement
+ */
+int main(void)
+{
+	int  x = 0;
+
+	while (x < 10)
+	{
+		putchar (x + '0');
+		putchar (',');
+		putchar (' ');
+		x++;
+	}
+	putchar ('\n');
+
+	return (0);
+}
