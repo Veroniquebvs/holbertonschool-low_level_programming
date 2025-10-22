707b509bd3d90db71f75fc2c498925cd0236231d
veroniquebvs <beauvais.veronique@gmail.com>
2025-10-22 10:21:52 +0200

    create programm that prints all the numbers of base 16

---
+++ variables_if_else_while/8-print_base16.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - point d'entrée du programme
+ * Description: affiche nombre hexadecimal
+ * Return: 0 si le programme s'exécute correctement
+ */
+int main(void)
+{
+	char hex[] = "0123456789abcdef";
+	int i = '0';
+
+	while (i < 16)
+	{
+		putchar (hex[i++]);
+		}
+	putchar ('\n');
+
+	return (0);
+}
