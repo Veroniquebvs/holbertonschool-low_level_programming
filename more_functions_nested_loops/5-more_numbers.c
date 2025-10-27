e1a90c847f32ae7f9e773d1a8af19e5ac4b0a00f
veroniquebvs <beauvais.veronique@gmail.com>
2025-10-27 14:33:35 +0100

    create functin that prints 10 times the numbers from 0 to 14

---
+++ more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,33 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * more_numbers - Affiche les chiffres de 0 à 14, 10 fois
+ */
+
+void more_numbers(void)
+{
+	int i, number;
+
+	for (i = 0; i < 10 ; i++)
+	{
+		for (number = 0; number <= 14; number++)
+		{
+
+			if (number <= 9)
+			{
+				return (number + '0');
+			}
+
+			else
+			{
+				_putchar(number / 10 + '0');
+				_putchar(number % 10 + '0');
+			}
+
+		}
+		_putchar('\n');
+	}
+
+
+}
