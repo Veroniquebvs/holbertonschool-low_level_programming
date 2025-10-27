f654127e37491d606cf9f2fc04bc58a2115a6a70
veroniquebvs <beauvais.veronique@gmail.com>
2025-10-27 11:05:29 +0100

    create function that mulptiplies two integers

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * mul - additionne deux entiers et affiche le résultat avec _putchar
+ * @a: premier entier
+ * @b: deuxième entier
+ *
+ * Return: la multiplication des deux entiers
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
