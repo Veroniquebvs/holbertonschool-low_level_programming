cb19a539ec8c3d3199f5f6e89dfb58d1713f5727
veroniquebvs <beauvais.veronique@gmail.com>
2025-10-27 15:15:52 +0100

    fix : create functin that prints 10 times the numbers from 0 to 14

--- more_functions_nested_loops/5-more_numbers.c
+++ more_functions_nested_loops/5-more_numbers.c
@@ -13,12 +13,14 @@ void more_numbers(void)
 	{
 		for (number = 0; number <= 14; number++)
 		{
-			_putchar (number + '0');
+			
 
 			if (number > 9)
 			{
-				_putchar (number % '0');
+				_putchar ('1');
 			}
+
+			_putchar (number % 10 + '0');
 
 		}
 		_putchar('\n');
