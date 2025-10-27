1a97af452b7b47a7999ed1816a82dd03862c90d9
veroniquebvs <beauvais.veronique@gmail.com>
2025-10-27 21:53:08 +0100

    create function that prints diagonal

---
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,33 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_diagonal - Affiche le charactère '\'
+ * @n: Le nombre de fois où le charactère '\' sera affiché
+ *
+ */
+
+void print_diagonal(int n)
+{
+	int i, j;
+
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+
+	else
+	{
+		for (i = 0; i < n; i++)
+		{
+			for (j = 0; j < i; j++)
+			{
+				_putchar(' ');
+			}
+
+			_putchar('\\');
+			_putchar('\n');
+		}
+
+	}
+}
