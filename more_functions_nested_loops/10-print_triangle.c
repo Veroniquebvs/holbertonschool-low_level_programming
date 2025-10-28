209fc0b9d62e40abaeb11d5a5c9007ac2d45f35d
veroniquebvs <beauvais.veronique@gmail.com>
2025-10-28 11:19:29 +0100

    create function that prints triangle

---
+++ more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * print_triangle - Affiche le charactère '#'
+ * @n: Le nombre de fois où le charactère '#' sera affiché
+ *
+ */
+
+void print_triangle(int n)
+{
+	int i, j;
+
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+
+	else
+	{
+		for (i = 0 ; i < n ; i++)
+		{
+		for (j = 0 ; j < (n - 1 - i) ; j++)
+		{
+			_putchar(' ');
+		}
+		for (j = 0 ; j < (i + 1) ; j++)
+		{
+			_putchar('#');
+		}
+		_putchar('\n');
+		}
+
+	}
+}
