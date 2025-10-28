bf49bb3a2f3488b1438db78a9dd7a47a760eef4c
veroniquebvs <beauvais.veronique@gmail.com>
2025-10-28 21:05:43 +0100

    create function that pointer changes value

---
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * reset_to_98 - prend la valeur de 98
+ * @n: Le nombre de fois où le charactère '#' sera affiché en triangle
+ *
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
