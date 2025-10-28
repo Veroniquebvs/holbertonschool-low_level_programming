0e42dc6145de291847bb506af780ba3f67301dd9
veroniquebvs <beauvais.veronique@gmail.com>
2025-10-28 09:36:36 +0100

    create function that prints number from 1 to 100 or Fizz or Buzz or FizzBuzz

---
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,43 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - point d'entrée du programme
+ * Description : affiche les nombres de 1 à 100,
+ * Si multiple de 3 : affiche Fizz,
+ * Si multiple de 5 : affiche Buzz,
+ * Si multiple de 3 et 5 : affiche FizzBuzz
+ * Return: 0 si le programme s'exécute correctement
+ */
+
+int main(void)
+{
+	int i = 1;
+
+	for (i = 1; i <= 100; i++)
+	{
+		if (i % 3 == 0)
+		{
+			printf("Fizz ");
+		}
+
+		else if (i % 5 == 0)
+		{
+			printf("Buzz ");
+		}
+
+		else if (i % 3 == 0 && i % 5 == 0)
+		{
+			printf("FizzBuzz ");
+		}
+
+		else
+		{
+			printf("%d ", i);
+		}
+
+	}
+	printf("\n");
+
+	return (0);
+}
