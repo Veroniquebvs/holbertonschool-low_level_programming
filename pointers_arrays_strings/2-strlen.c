ca4ff10caee291a554c3ef7af7053c09ee58756e
veroniquebvs <beauvais.veronique@gmail.com>
2025-10-28 22:37:29 +0100

    create function that prints lengh on a string

---
+++ pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _strlen - renvoie la longueur d'une chaine
+ * @s: Le nombre à échanger
+ *
+ *Return: la longueur de la chaine
+ */
+
+int _strlen(char *s)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+
+	}
+
+	return (i);
+}
