83fcdfb3d08a3618463bfb1087b53c2b8806e558
veroniquebvs <beauvais.veronique@gmail.com>
2025-11-03 21:51:28 +0100

    creat function that searches a string

---
+++ pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * *_strpbrk - recherche un caractère
+ * @s: chaîne à utiliser pour la recherche
+ * @accept: liste des caractères recherché
+ *
+ *Return: un pointeur vers le premier caractère de la liste rencontré
+ */
+
+char *_strpbrk(char *s, char *accept)
+{
+	unsigned int i, j;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (j = 0; accept[j] != '\0'; j++)
+		{
+			if (s[i] == accept[j])
+			return (&s[i]);
+		}
+	}
+
+	return (NULL);
+
+}
