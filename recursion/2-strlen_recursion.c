95d6543ec39a6513bfa25e65ffb5e7fe56450626
veroniquebvs <beauvais.veronique@gmail.com>
2025-11-05 13:51:25 +0100

    create recursive function that prints length of a string

---
+++ recursion/2-strlen_recursion.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * *_strlen_recursion - mesure la longueur d'une chaîne
+ * @s: chaîne à mesurer
+ *
+ *Return: la longueur de la chaîne
+ */
+
+int _strlen_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		return (0);
+	}
+
+	_strlen_recursion(s + 1);
+	return (_strlen_recursion(s + 1) + 1);
+
+}
