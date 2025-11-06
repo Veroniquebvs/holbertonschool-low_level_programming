fa713c9b1af9412a91d135a651cb43297dab9bc2
veroniquebvs <beauvais.veronique@gmail.com>
2025-11-06 18:19:35 +0100

    create function that prints name of the file

---
+++ argc_argv/0-whatsmyname.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+* main - point d'entrée du programme
+* @argc: nombre total d'arguments
+* @argv: tableau contenant les arguments
+* Return: 0 si le programme s'exécute correctement
+*/
+int main(__attribute__((unused)) int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
