4b519ae29140455b68a7061ef1df3c74c01e6b2b
veroniquebvs <beauvais.veronique@gmail.com>
2025-11-13 15:03:00 +0100

    Addition dog structure

---
+++ structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/**
+ * struct dog - describe a dog
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: name of the owner
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+
+#endif
