d6028b594d7ab9b89d1c36edf4a343ebaee2a4cc
veroniquebvs <beauvais.veronique@gmail.com>
2025-11-14 14:49:51 +0100

    Create function that frees memory

---
+++ structures_typedef/5-free_dog.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "dog.h"
+
+/**
+ * free_dog - libère de la mémoire
+ * @d: données de la variable
+ * Return: void
+ */
+
+void free_dog(dog_t *d)
+{
+	free(d->owner);
+	free(d->name);
+	free(d);
+}
