ea929d00f846f0408cf24ec9ade7a120e1bc9eed
veroniquebvs <beauvais.veronique@gmail.com>
2025-11-17 17:22:27 +0100

    Create function that execute a function given as a parameter

---
+++ function_pointers/1-array_iterator.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "function_pointers.h"
+
+
+/**
+ * array_iterator - execute a function given as a parameter
+ * @array: array
+ * @size: size of the array
+ * @action: pointer a function
+ *
+ * Return: nothing
+ */
+
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	size_t i;
+
+	for (i = 0; i < size; i++)
+	{
+		action(array[i]);
+	}
+}
