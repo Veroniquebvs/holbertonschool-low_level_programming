39fb2d7576b636c003131e747c23e1beda802697
veroniquebvs <beauvais.veronique@gmail.com>
2025-11-17 16:28:25 +0100

    Create function that print a name with a function pointer

---
+++ function_pointers/0-print_name.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "function_pointers.h"
+
+
+/**
+ * print_name - prints a name as is
+ * @name: name of the person
+ * @f: pointer a function
+ *
+ * Return: void
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}
