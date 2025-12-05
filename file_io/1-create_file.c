1eb0c6c66eb2ffbd1a6bcdd5c94d3fb3a2acd351
veroniquebvs <beauvais.veronique@gmail.com>
2025-12-05 18:32:20 +0100

    fix : modification a return 0 in return -1

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -16,7 +16,7 @@ int create_file(const char *filename, char *text_content)
 
 	if (filename == NULL)
 	{
-		return (0);
+		return (-1);
 	}
 
 	fic = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
